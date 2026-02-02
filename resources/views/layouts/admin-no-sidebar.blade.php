dae4dbd337b9c81313d8bab6b046e7b0c6cae0f7
Vijja Setiadi <vijjasetiadi23@gmail.com>
2026-02-02 08:59:15 +0700

    layouting

--- resources/views/layouts/admin-no-sidebar.blade.php
+++ resources/views/layouts/admin-no-sidebar.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Dashboard') - KKN International</title>
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
